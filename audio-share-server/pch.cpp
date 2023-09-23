9505f30441426189373b4c23cbde89c1464dfe7b
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-23 17:14:37 +0800

    add link to license notice

--- audio-share-server/pch.cpp
+++ audio-share-server/pch.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2022-2023 mkckr0
+   Copyright 2022-2023 mkckr0 <https://github.com/mkckr0>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
