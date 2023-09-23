76bd18936985fb648de1a8d50ab1992aaf51c16f
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-23 16:23:40 +0800

    add license to source code

--- audio-share-server/pch.h
+++ audio-share-server/pch.h
@@ -1,4 +1,20 @@
-﻿// pch.h: 这是预编译标头文件。
+﻿/*
+   Copyright 2022-2023 mkckr0
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// pch.h: 这是预编译标头文件。
 // 下方列出的文件仅编译一次，提高了将来生成的生成性能。
 // 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
 // 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
