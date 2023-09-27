e01129164d6f9dc31f1c656f305b94602bd0a0b3
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-27 15:59:46 +0800

    update version

--- audio-share-app/app/build.gradle.kts
+++ audio-share-app/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "io.github.mkckr0.audio_share_app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
+        versionCode = 9
         versionName = "0.0.9"
         archivesName.set("${rootProject.name}-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- script/apply_version.sh
+++ script/apply_version.sh
@@ -2,12 +2,21 @@ pushd $(dirname $0)/.. &>/dev/null
 
 version_name=$(sed -n '1p' VERSION)
 version_code=$(sed -n '2p' VERSION)
+file_version0=$version_name.0
+file_version1=$(echo $file_version0 | sed -En 's|\.|,|gp')
+product_version=$file_version1
 
-echo VERSION: version_name=$version_name version_code=$version_code
+echo VERSION: \
+    version_name=$version_name \
+    version_code=$version_code
 
-sed -Ei "s|versionName\s*=\s*\"([^\"]*)\"|versionName = \"$version_name\"|g" audio-share-app/app/build.gradle.kts
-sed -Ei "s|versionCode\s*=\s*([0-9]*)|versionCode = $version_code|g" audio-share-app/app/build.gradle.kts
+sed -Ebi "s|versionName\s*=\s*\"[^\"]*\"|versionName = \"$version_name\"|g" audio-share-app/app/build.gradle.kts
+sed -Ebi "s|versionCode\s*=\s*[0-9]*|versionCode = $version_code|g" audio-share-app/app/build.gradle.kts
 
-sed -Ei "s|Audio Share Server, Version ([^\"]*)|Audio Share Server, Version $version_name|g" audio-share-server/AudioShareServer.rc
+sed -Ebi "s|Audio Share Server, Version [^\"]*|Audio Share Server, Version $version_name|g" audio-share-server/AudioShareServer.rc
+sed -Ebi "s|FILEVERSION [0-9,]*|FILEVERSION $file_version1|g" audio-share-server/AudioShareServer.rc
+sed -Ebi "s|PRODUCTVERSION [0-9,]*|PRODUCTVERSION $product_version|g" audio-share-server/AudioShareServer.rc
+sed -Ebi "s|\"FileVersion\", \"[^\"]*\"|\"FileVersion\", \"$version_name.0\"|g" audio-share-server/AudioShareServer.rc
+sed -Ebi "s|\"ProductVersion\", \"[^\"]*\"|\"ProductVersion\", \"$version_name\"|g" audio-share-server/AudioShareServer.rc
 
 popd &>/dev/null
