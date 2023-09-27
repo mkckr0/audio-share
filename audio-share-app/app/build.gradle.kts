1c7ada6cd0dc2a23024307ac07cdbd24c7f3d7d1
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-27 14:32:19 +0800

    change .rc to UTF-8

--- audio-share-app/app/build.gradle.kts
+++ audio-share-app/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.github.mkckr0.audio_share_app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.8"
+        versionCode = 6
+        versionName = "0.0.9"
         archivesName.set("${rootProject.name}-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
