786d3db1b08caeb3cbe25fbdf8df214ae0ba791f
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-27 18:02:06 +0800

    Revert "Apply Version 0.0.9"
    This reverts commit 961ec7c3b022e5d6d8265d2e213a44ead35d2c81.

--- audio-share-app/app/build.gradle.kts
+++ audio-share-app/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.github.mkckr0.audio_share_app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 
-        versionName = ""
+        versionCode = 9
+        versionName = "0.0.9"
         archivesName.set("${rootProject.name}-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
