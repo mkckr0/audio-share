9a960b2705fc05ebb82162c57b7cb146e569d21c
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-09-27 15:33:03 +0800

    update rc

--- script/apply_version.sh
+++ script/apply_version.sh
@@ -5,13 +5,9 @@ version_code=$(sed -n '2p' VERSION)
 
 echo VERSION: version_name=$version_name version_code=$version_code
 
-echo audio-share-app/app/build.gradle.kts: \
-    $(sed -En 's|versionName\s*=\s*"([^"]*)"|\0|p' audio-share-app/app/build.gradle.kts) \
-    $(sed -En 's|versionCode\s*=\s*([0-9]*)|\0|p' audio-share-app/app/build.gradle.kts)
-
 sed -Ei "s|versionName\s*=\s*\"([^\"]*)\"|versionName = \"$version_name\"|g" audio-share-app/app/build.gradle.kts
 sed -Ei "s|versionCode\s*=\s*([0-9]*)|versionCode = $version_code|g" audio-share-app/app/build.gradle.kts
 
-echo $(sed -En 's|"Audio Share Server|\0|p' audio-share-server/AudioShareServer.rc)
+sed -Ei "s|Audio Share Server, Version ([^\"]*)|Audio Share Server, Version $version_name|g" audio-share-server/AudioShareServer.rc
 
 popd &>/dev/null
