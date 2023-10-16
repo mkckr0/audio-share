a1011619a89630039e8420b035cb815b522e19fc
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2023-10-16 14:20:02 +0800

    keep PCM sample always be LE

--- client-android/app/src/main/java/io/github/mkckr0/audio_share_app/NetClient.kt
+++ client-android/app/src/main/java/io/github/mkckr0/audio_share_app/NetClient.kt
@@ -33,8 +33,6 @@ import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.ChannelOption
 import io.netty.channel.ConnectTimeoutException
-import io.netty.channel.epoll.EpollChannelConfig
-import io.netty.channel.epoll.EpollChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.DatagramChannel
 import io.netty.channel.socket.DatagramPacket
@@ -231,7 +229,8 @@ class NetClient(private val handler: Handler, private val application: Applicati
                 val buf = ByteBuffer.allocate(msg.content().readableBytes())
                 msg.content().readBytes(buf)
                 buf.flip()
-                val floatBuffer = buf.order(ByteOrder.nativeOrder()).asFloatBuffer()
+                // keep PCM sample always be LE.
+                val floatBuffer = buf.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer()
                 val floatArray = FloatArray(floatBuffer.capacity())
                 floatBuffer.get(floatArray)
                 out.add(floatArray)
@@ -399,6 +398,8 @@ class NetClient(private val handler: Handler, private val application: Applicati
             tag,
             "encoding: $encoding, channelMask: $channelMask, sampleRate: ${format.sampleRate}"
         )
+
+        Log.i(tag, "native order: ${ByteOrder.nativeOrder()}")
 
         val minBufferSize = AudioTrack.getMinBufferSize(format.sampleRate, channelMask, encoding)
         val bufferSizeScale = sharedPreferences.getString(
