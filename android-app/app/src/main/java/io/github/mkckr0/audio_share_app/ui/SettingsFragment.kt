cc5a8f7ef6453d9d839347b4f95fdf225b72cfd6
Michael Camarata <CamarataM@outlook.com>
2023-11-12 00:12:44 +0000

    Add switch preference to SettingsFragment.kt

--- android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/SettingsFragment.kt
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/SettingsFragment.kt
@@ -1,5 +1,6 @@
 /**
  *    Copyright 2022-2023 mkckr0 <https://github.com/mkckr0>
+ *    Copyright 2023 CamarataM <https://github.com/CamarataM>
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -56,6 +57,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 it.setSelection(it.length())
             }
             setSummaryProvider { "${(it as EditTextPreference).text}ms" }
+        }
+        findPreference<SwitchPreference>("audio_use_volume_sliders")!!.apply {
+            isEnabled = true
         }
 
         updateRequestIgnoreBatteryOptimizations()
