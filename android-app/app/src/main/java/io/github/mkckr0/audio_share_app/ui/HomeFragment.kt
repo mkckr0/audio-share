915f308af88718874f00f0703bb81a881ed77e5f
Michael Camarata <CamarataM@outlook.com>
2023-11-12 00:18:32 +0000

    Fix copyright formatting in HomeFragment.kt

--- android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/HomeFragment.kt
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/HomeFragment.kt
@@ -1,6 +1,6 @@
 /**
  *    Copyright 2022-2023 mkckr0 <https://github.com/mkckr0>
-*    Copyright 2023 CamarataM <https://github.com/CamarataM>
+ *    Copyright 2023 CamarataM <https://github.com/CamarataM>
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
