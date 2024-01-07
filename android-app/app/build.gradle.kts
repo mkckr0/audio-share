9b44c083587f5b0a80e3e3510db7a75ca54010ed
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2024-01-07 22:13:15 +0800

    v0.0.15

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.github.mkckr0.audio_share_app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 14
-        versionName = "0.0.14"
+        versionCode = 15
+        versionName = "0.0.15"
         archivesName.set("${rootProject.name}-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
