aadeefe64b721a7b0eed41b77dbaf0f5ed872220
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2024-04-28 18:05:03 +0800

    compare newer version

---
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/Util.kt
@@ -0,0 +1,20 @@
+package io.github.mkckr0.audio_share_app
+
+object Util {
+    fun isNewerVersion(a: String, b: String): Boolean {
+        if (a[0] != 'v' || b[0] != 'v') {
+            throw IllegalArgumentException("version format error, a=${a} b=${b}")
+        }
+        val aList = a.substring(1).split('.').map { it.toInt() }
+        val bList = b.substring(1).split('.').map { it.toInt() }
+        if (aList.size != 3 || bList.size != 3) {
+            throw IllegalArgumentException("version format error, a=${a} b=${b}")
+        }
+        aList.forEachIndexed { index, i ->
+            if (i > bList[index]) {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ android-app/app/src/test/java/io/github/mkckr0/audio_share_app/UtilTest.kt
@@ -0,0 +1,47 @@
+package io.github.mkckr0.audio_share_app
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class UtilTest {
+
+    @Test
+    fun isNewerVersion_arg0() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Util.isNewerVersion("3.2.2", "v0.0.1")
+        }
+    }
+
+    @Test
+    fun isNewerVersion_arg1() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Util.isNewerVersion("v3.2.", "v0.0.")
+        }
+    }
+
+    @Test
+    fun isNewerVersion_version0() {
+        assertTrue(Util.isNewerVersion("v0.0.17", "v0.0.9"))
+    }
+
+    @Test
+    fun isNewerVersion_version1() {
+        assertTrue(Util.isNewerVersion("v0.1.0", "v0.0.17"))
+    }
+
+    @Test
+    fun isNewerVersion_version2() {
+        assertTrue(Util.isNewerVersion("v0.17.0", "v0.9.17"))
+    }
+
+    @Test
+    fun isNewerVersion_version3() {
+        assertTrue(Util.isNewerVersion("v12.17.0", "v1.0.0"))
+    }
+
+    @Test
+    fun isNewerVersion_version4() {
+        assertFalse(Util.isNewerVersion("v12.17.0", "v12.17.0"))
+    }
+}
