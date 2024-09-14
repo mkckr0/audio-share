4cda4eef2681d0417b41b688f911a506af81a622
mkckr0 <26296627+mkckr0@users.noreply.github.com>
2024-09-14 13:32:17 +0800

    gradle migrate to version catalogs

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,12 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    val kotlinVersion = "1.9.23"
-
-    id("com.android.application") version "8.6.0" apply false
-    kotlin("android") version kotlinVersion apply false
-    kotlin("kapt") version kotlinVersion apply false
-    kotlin("plugin.serialization") version kotlinVersion apply false
-    kotlin("plugin.parcelize") version kotlinVersion apply false
-    id("com.google.protobuf") version "0.9.4" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.kotlin.plugin.parcelize) apply false
+    alias(libs.plugins.protobuf) apply false
 }
