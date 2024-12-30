c2ed667989a3dc7aa434e53d2b7d7cb0523f453f
ftb2519 <ftb2519@gmail.com>
2024-12-30 21:06:59 +0300

    Update PlaybackService.kt

--- android-app/app/src/main/java/io/github/mkckr0/audio_share_app/service/PlaybackService.kt
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/service/PlaybackService.kt
@@ -1,19 +1,3 @@
-/*
- *    Copyright 2022-2024 mkckr0 <https://github.com/mkckr0>
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package io.github.mkckr0.audio_share_app.service
 
 import android.app.PendingIntent
@@ -27,11 +11,6 @@ import androidx.annotation.OptIn
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.CommandButton
-import androidx.media3.session.CommandButton.ICON_STOP
-import androidx.media3.session.CommandButton.SLOT_BACK
-import androidx.media3.session.CommandButton.SLOT_BACK_SECONDARY
-import androidx.media3.session.CommandButton.SLOT_FORWARD
-import androidx.media3.session.CommandButton.SLOT_FORWARD_SECONDARY
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSession.ConnectionResult.AcceptedResultBuilder
 import androidx.media3.session.MediaSession.ConnectionResult.DEFAULT_SESSION_COMMANDS
@@ -50,9 +29,11 @@ class PlaybackService : MediaSessionService() {
 
     companion object {
         const val ACTION_STOP_SERVICE = "action_stop_service"
+        const val ACTION_DISABLE_MUSIC_INFO = "action_disable_music_info"
     }
 
     private val customCommandStop = SessionCommand(ACTION_STOP_SERVICE, Bundle.EMPTY)
+    private val customCommandDisableMusicInfo = SessionCommand(ACTION_DISABLE_MUSIC_INFO, Bundle.EMPTY)
 
     @OptIn(UnstableApi::class)
     override fun onCreate() {
@@ -75,10 +56,10 @@ class PlaybackService : MediaSessionService() {
                     CommandButton.Builder(ICON_STOP)
                         .setSessionCommand(customCommandStop)
                         .setSlots(
-                            SLOT_BACK,
-                            SLOT_FORWARD,
-                            SLOT_BACK_SECONDARY,
-                            SLOT_FORWARD_SECONDARY
+                            CommandButton.SLOT_BACK,
+                            CommandButton.SLOT_FORWARD,
+                            CommandButton.SLOT_BACK_SECONDARY,
+                            CommandButton.SLOT_FORWARD_SECONDARY
                         )
                         .setDisplayName("Stop")
                         .setEnabled(true)
@@ -97,7 +78,9 @@ class PlaybackService : MediaSessionService() {
                     return AcceptedResultBuilder(session)
                         .setAvailableSessionCommands(
                             DEFAULT_SESSION_COMMANDS.buildUpon()
-                                .add(customCommandStop).build()
+                                .add(customCommandStop)
+                                .add(customCommandDisableMusicInfo)
+                                .build()
                         )
                         .build()
                 }
@@ -112,6 +95,10 @@ class PlaybackService : MediaSessionService() {
                         Log.d(tag, "ACTION_STOP_SERVICE")
                         session.player.stop()
                         stopSelf()
+                        return Futures.immediateFuture(SessionResult(SessionResult.RESULT_SUCCESS))
+                    } else if (customCommand.customAction == ACTION_DISABLE_MUSIC_INFO) {
+                        Log.d(tag, "ACTION_DISABLE_MUSIC_INFO")
+                        (session.player as AudioPlayer).disableMusicInfo()
                         return Futures.immediateFuture(SessionResult(SessionResult.RESULT_SUCCESS))
                     }
                     return super.onCustomCommand(session, controller, customCommand, args)
