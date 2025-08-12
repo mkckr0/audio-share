84582e84c286bf5aeaf6f13cac31c8c6616fe6cc
SubRighteous <danjrys@gmail.com>
2025-08-12 13:16:36 -0400

    Removing Unused Libraries
    These changes removed unused and unnecessary imports

--- android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/base/Config.kt
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/base/Config.kt
@@ -32,14 +32,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
