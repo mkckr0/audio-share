9ca458e2fb2745c11b4242b49a83897d6604e36a
SubRighteous <danjrys@gmail.com>
2025-08-12 13:05:41 -0400

    Fixes to the audio screen accessibility
    These changes improve accessibility in the Audio screen and should address issue #157.
    
    Fixes: https://github.com/mkckr0/audio-share/issues/157

--- android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/base/Config.kt
+++ android-app/app/src/main/java/io/github/mkckr0/audio_share_app/ui/base/Config.kt
@@ -32,12 +32,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.floatPreferencesKey
@@ -49,7 +55,12 @@ import kotlinx.coroutines.launch
 fun ConfigGroup(title: String, content: @Composable () -> Unit) {
     OutlinedCard(modifier = Modifier.fillMaxWidth()) {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp).semantics(
+                mergeDescendants = true
+            ){
+                isTraversalGroup = true
+                traversalIndex = 1f
+            },
         ) {
             Text(
                 text = title,
@@ -110,7 +121,9 @@ fun SliderConfig(
                 track = {
                     SliderDefaults.Track(it, drawTick = { _, _ -> })
                 },
-                modifier = Modifier.padding(top = 4.dp, start = 2.dp, end = 2.dp),
+                modifier = Modifier.padding(top = 4.dp, start = 2.dp, end = 2.dp).semantics {
+                    contentDescription = "$title " + valueFormatter(tempValue)
+                },
             )
         }
     }
